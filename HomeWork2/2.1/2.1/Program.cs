80627062a649b2049f02fe2b3f44bf48bba41b66
Celia <1830170706@qq.com>
2020-03-01 21:43:07 +0800

    Signed-off-by: Celia <1830170706@qq.com>

---
+++ HomeWork2/2.1/2.1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//编写程序输出用户指定数据的所有素数因子
+
+namespace ConsoleApp2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string n;
+			Console.WriteLine("输入一个整数:");
+			n = Console.ReadLine();
+			Console.WriteLine("它的所有素数因子为:");
+			int x = int.Parse(n);
+			while (x > -1)
+			{
+				int j = (int)Math.Sqrt(x);
+				int i = 0;
+				for (i = 2; i <= j; i++)
+				{
+					if (x % i == 0)
+					{
+						Console.WriteLine(i);
+						x = x / i;
+						break;
+					}
+				}
+				if (i > j)
+				{
+					Console.WriteLine(x);
+					break;
+				}
+			}
+		}
+
+
+	}
+}
---
+++ HomeWork2/2.2/2.2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//编程求一个整数数组的最大值、最小值、平均值和所有数组元素的和
+
+namespace _2._2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("请输入数组长度：");
+			int len = int.Parse(Console.ReadLine());
+			Console.WriteLine("请输入{0}个值", len);
+			//定义一个数组
+			int[] array = new int[len];
+			//给数据添加值
+			for (int i = 0; i < len; i++)
+			{
+				array[i] = Convert.ToInt32(Console.ReadLine());
+			}
+			
+			Console.Write("数组的最大值为" + array.Max()+"\n");
+			Console.Write("数组的最小值为" + array.Min() + "\n");
+			Console.Write("数组的平均值为" + array.Average() + "\n");
+			Console.Write("数组元素之和为" + array.Sum() + "\n");
+		}
+	}
+}
