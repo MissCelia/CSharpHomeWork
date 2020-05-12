202831ba879e452966649bc8e33dd076027f56c1
Celia <1830170706@qq.com>
2020-05-12 21:46:13 +0800

    Signed-off-by: Celia <1830170706@qq.com>

---
+++ HomeWork6/OrderManagement/OrderManagement/Program.cs
@@ -0,0 +1,34 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace OrderManagement
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			OrderItem item1 = new OrderItem("ship", 1, 1);
+			OrderItem item2 = new OrderItem("car", 1, 1);
+			OrderItem item3 = new OrderItem("battery", 1, 1);
+			OrderItem item4 = new OrderItem("ship", 1, 1);
+			List<OrderItem> itemlist1 = new List<OrderItem> {item1 };
+			List<OrderItem> itemlist2 = new List<OrderItem> { item2};
+			Order order1 = new Order(000, "Sam", itemlist1);
+			Order order2 = new Order(521, "Jam", itemlist2);
+			Order order3 = new Order(002, "Sara", itemlist2);
+
+
+			List<Order> orderlist = new List<Order> { order1,order2,order3};
+			OrderService newService = new OrderService(orderlist);
+			newService.OrderSort();
+			newService.OrderChack();
+
+			//newService.Export("order.xml");
+			//newService.Import("order.xml");
+		}
+	}
+}
---
+++ HomeWork6/OrderManagement/OrderManagementTests/OrderServiceTests.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OrderManagement;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.IO;
+
+namespace OrderManagement.Tests
+{
+	[TestClass()]
+	public class OrderServiceTests
+	{
+		OrderService newService = new OrderService();
+		OrderItem item1 = new OrderItem("ship", 1, 1);
+		OrderItem item2 = new OrderItem("car", 1, 1);
+		OrderItem item3 = new OrderItem("battery", 1, 1);
+		OrderItem item4 = new OrderItem("ship", 1, 1);
+
+		[TestInitialize()]
+		public void init()
+		{
+			Order order1 = new Order(521, "Sam", new List<OrderItem> { item1, item2 });
+			Order order2 = new Order(000, "Jam", new List<OrderItem> { item3 });
+			newService = new OrderService();
+			newService.OrderAdd(order1);
+			newService.OrderAdd(order2);
+		}
+
+
+		[TestMethod()]
+		public void OrderAddTest()
+		{
+			Order order3 = new Order(002, "Sara", new List<OrderItem> { item4 });
+			newService.OrderAdd(order3);
+			CollectionAssert.Contains(newService.OrderList, order3);
+		}
+
+
+
+		[TestMethod()]
+		public void OrderSortTest()
+		{
+			newService.OrderSort();
+			Assert.AreEqual(newService.OrderList[1].OrderNumber, 521);
+			Assert.AreEqual(newService.OrderList[0].OrderNumber, 000);
+		}
+
+
+
+		[TestMethod()]
+		public void OrderModifyTest()
+		{
+			Order order2 = new Order(521, "newJam", new List<OrderItem> { item3, item4 });
+			newService.OrderModify(order2);
+
+			Assert.AreEqual(order2.Customer, newService.GetOrder(521).Customer);
+		}
+
+
+
+		[TestMethod()]
+		public void OrderDelTest()
+		{
+			newService.OrderDel(002);
+			Assert.AreEqual(2, newService.OrderList.Count());
+			//Assert.IsNull(newService.OrderQuery(OrderService.Attribute.OrderNumber, "002"));
+		}
+
+
+		[TestMethod()]
+		public void OrderQueryTest()
+		{
+			List<Order> o= newService.OrderQuery(OrderService.Attribute.CustomerName, "Sam");
+			Assert.AreEqual(o[0].OrderNumber, 521);
+		}
+	}
+}
