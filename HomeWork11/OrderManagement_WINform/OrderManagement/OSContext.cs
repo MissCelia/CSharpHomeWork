a08b49567d6ae6e6e3e61caa16618190ec7b19e8
Celia <1830170706@qq.com>
2020-05-27 18:59:28 +0800

    Signed-off-by: Celia <1830170706@qq.com>

---
+++ HomeWork11/OrderManagement_WINform/OrderManagement/OSContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace OrderManagement
+{
+	public class OSContext : DbContext
+	{
+		public OSContext() : base("OSDataBase")
+		{
+			Database.SetInitializer(
+				new DropCreateDatabaseIfModelChanges<OSContext>());
+		}
+
+	
+		public DbSet<OrderItem> OrderItems { get; set; }
+		public DbSet<Order> Orders { get; set; }
+	}
+}
---
+++ HomeWork11/OrderManagement_WINform/OrderManagement_WINform/Form3.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OrderManagement;
+
+namespace OrderManagement_WINform
+{
+	public partial class Form3 : Form
+	{
+		
+		public OrderItem CurrentItem;
+		public Form3()
+		{
+			InitializeComponent();
+			CurrentItem = new OrderItem();
+			itemBindingSource.DataSource = CurrentItem;
+		}
+
+
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			//if (CurrentItem == null)
+			//{
+				//CurrentItem.Name = textBox1.Text;
+				//CurrentItem.Price = double.Parse(textBox2.Text);
+				//CurrentItem.Quantity = int.Parse(textBox3.Text);
+				//double temp = CurrentItem.Price * CurrentItem.Quantity;
+				//textBox4.Text = temp.ToString();
+			//}
+			this.Close();
+			
+		}
+	}
+}
